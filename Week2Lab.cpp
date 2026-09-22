e8c514e1b95265aa0e89807c8ba92449b15d7e50
fraty-mjc <erik791425@my.yosemite.edu>
2026-09-22 15:56:47 -0700

    Add files via upload

---
+++ Week2Lab.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int luckyNumber;
+    luckyNumber = 6;
+
+    cout << "My lucky number is: " << luckyNumber << endl;
+
+    return 0;
+}
