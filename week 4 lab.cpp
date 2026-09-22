6d5a9f65af4ec36df8d7feefa958432d66c090e5
fraty-mjc <erik791425@my.yosemite.edu>
2026-09-22 15:54:54 -0700

    Add files via upload

---
+++ week 4 lab.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    int month, day;
+    string zodiac;
+
+    cout << "Enter full name: ";
+    getline(cin, name);
+
+    cout << "Enter birth month (1-12): ";
+    cin >> month;
+
+    cout << "Enter birth day: ";
+    cin >> day;
+
+    if ((month == 3 && day >= 21) || (month == 4 && day <= 19))
+    zodiac = "Aries";
+else if ((month == 4 && day >= 20) || (month == 5 && day <= 20))
+    zodiac = "Taurus";
+else if ((month == 5 && day >= 21) || (month == 6 && day <= 20))
+    zodiac = "Gemini";
+else if ((month == 6 && day >= 21) || (month == 7 && day <= 22))
+    zodiac = "Cancer";
+
+else if ((month == 7 && day >= 23) || (month == 8 && day <= 22))
+zodiac = "Leo";
+else if ((month == 8 && day >= 23) || (month == 9 && day <= 22))
+zodiac = "Virgo";
+else if ((month == 9 && day >= 23) || (month == 10 && day <= 22))
+zodiac = "Libra";
+else if ((month == 10 && day >= 23) || (month == 11 && day <= 21))
+zodiac = "Scorpio";
+
+else if ((month == 11 && day >= 22) || (month == 12 && day <= 21))
+zodiac = "Sagittarius";
+else if ((month == 12 && day >= 22) || (month == 1 && day <= 19))
+zodiac = "Capricorn";
+else if ((month == 1 && day >= 20) || (month == 2 && day <= 18))
+zodiac = "Aquarius";
+else
+zodiac = "Pisces";
+
+cout << name << ", your zodiac sign is " << zodiac << endl;
+return 0;
+}
